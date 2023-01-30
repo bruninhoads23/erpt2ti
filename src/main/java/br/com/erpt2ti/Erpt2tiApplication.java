6f509b95ecf2bf7c7ec83f428806600f702c6c1c
Bruno <Bruno@Bruno-PC>
2023-01-30 19:58:13 -0300

    commit inicial do projeto erp

---
+++ src/main/java/br/com/erpt2ti/Erpt2tiApplication.java
@@ -0,0 +1,13 @@
+package br.com.erpt2ti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Erpt2tiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Erpt2tiApplication.class, args);
+	}
+
+}
